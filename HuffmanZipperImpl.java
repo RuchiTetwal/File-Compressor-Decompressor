ded89b28694f8ed8f78fa866a137dffceadc12f4
Ruchi Tetwal <ruchi2452002@gmail.com>
2023-01-26 10:47:44 +0530

    added compressin and decompression time

--- HuffmanZipperImpl.java
+++ HuffmanZipperImpl.java
@@ -1,20 +1,32 @@
 import java.io.*;
+import java.util.logging.*;
 
 import compressor.*;
 import decompressor.*;
 
 public class HuffmanZipperImpl implements IFileZipper{
 
+    Logger logger = Logger.getLogger(HuffmanDecompressorAlgoImpl.class.getName());
+
     @Override
     public void compress(String originalFileName, String compFileName) throws IOException{
         ICompressor compObj = new HuffmanCompressorImpl();
-        compObj.compress(originalFileName, compFileName);       
+        long startTimeC= System.currentTimeMillis();
+        compObj.compress(originalFileName, compFileName);     
+        long endTimeC= System.currentTimeMillis();  
+        String msg= String.format("Compression time : %d",(endTimeC-startTimeC));
+        logger.log(Level.INFO, msg );
+
     }
 
     @Override
     public void decompress(String compFileName,String originalFileName, String decompFileName) throws IOException{
         IDecompressor decompObj = new HuffmanDecompressorImpl();
+        long startTimeD= System.currentTimeMillis();
         decompObj.decompress(compFileName,originalFileName, decompFileName);
+        long endTimeD= System.currentTimeMillis();
+        String msg= String.format("Decompression time : %d",(endTimeD-startTimeD));
+        logger.log(Level.INFO, msg );
     }
     
 }
